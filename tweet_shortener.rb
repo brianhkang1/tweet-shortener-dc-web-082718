3a33e4055200fa151923c18ba5071fea4f57aba1
Brian Kang <brianhkang1@gmail.com>
2018-08-15 23:55:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,12 @@
 def dictionary
-  {"hello" => "hi"
-  "to, two, too" => "2"
-  "for, four" => "4"
-  "be" => "b"
-  "you" => "u"
-  "at" => "@"
+  {"hello" => "hi",
+  "to" => "2"
+  "two" => "2",
+  "too" => "2",
+  "for, four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
   "and" => "&"
   }
 end
