edba02a7da48e2b84a1cbdfc3b44e4335d20b833
Brian Kang <brianhkang1@gmail.com>
2018-08-16 00:15:18 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,9 @@ def bulk_tweet_shortener(array_of_tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 130
+  if tweet.length > 140
     word_substituter(tweet)
+  else 
+    tweet 
   end
 end
